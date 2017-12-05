8f6da9b9d0581cca477a09b420914d42e55d5437
DanBonix <danheavydrive@gmail.com>
2017-12-05 19:50:55 +0100

    Add files via upload

---
+++ C#/Windows Forms/[WF] TP_Mere_Fille/MereFillev2/FFille2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MereFillev2
+{
+    public partial class FFille : Form
+    {
+        private string monNom;
+        private FMere2 maMere;
+
+        public FFille(FMere2 maMere, int numero)
+        {
+            InitializeComponent();
+            this.Text = "Fille n° " + numero;
+            this.monNom = this.Text;
+            this.maMere = maMere;
+            btnMaMere.Click += new EventHandler(btnMaMere_Click);
+            this.FormClosing += new FormClosingEventHandler(FFille_FormClosing);
+            this.Load += new EventHandler(FFille_Load);
+            btnChangeNom.Click += new EventHandler(btnChangeNom_Click);
+        }
+
+        void btnChangeNom_Click(object sender, EventArgs e)
+        {
+            this.ChangerNom(tbNomFille.Text);
+        }
+
+        void FFille_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Evènement Load sur" + this.monNom);
+        }
+
+        void FFille_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = false;
+            MessageBox.Show("Tour désactivée");
+        }
+
+        void btnMaMere_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(maMere.NomMere);
+        }
+
+        private void ChangerNom(string nouveauNom)
+        {
+            this.monNom = nouveauNom;
+            this.Text = tbNomFille.Text;
+        }
+    }
+}
---
+++ C#/Windows Forms/[WF] TP_Mere_Fille/MereFillev2/FMere2.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MereFillev2
+{
+    partial class FMere2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lbLesFilles = new System.Windows.Forms.ListBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnShowDialog = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(35, 12);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 1;
+            this.btnNew.Text = "New()";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(175, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lbLesFilles
+            // 
+            this.lbLesFilles.FormattingEnabled = true;
+            this.lbLesFilles.Location = new System.Drawing.Point(35, 41);
+            this.lbLesFilles.Name = "lbLesFilles";
+            this.lbLesFilles.Size = new System.Drawing.Size(215, 134);
+            this.lbLesFilles.TabIndex = 3;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(35, 181);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            // 
+            // btnShowDialog
+            // 
+            this.btnShowDialog.Location = new System.Drawing.Point(35, 210);
+            this.btnShowDialog.Name = "btnShowDialog";
+            this.btnShowDialog.Size = new System.Drawing.Size(75, 23);
+            this.btnShowDialog.TabIndex = 6;
+            this.btnShowDialog.Text = "ShowDialog";
+            this.btnShowDialog.UseVisualStyleBackColor = true;
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(175, 181);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(75, 23);
+            this.btnHide.TabIndex = 7;
+            this.btnHide.Text = "Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            // 
+            // FMere
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.btnShowDialog);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lbLesFilles);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnNew);
+            this.Name = "FMere";
+            this.Text = "FMere";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListBox lbLesFilles;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnShowDialog;
+        private System.Windows.Forms.Button btnHide;
+    }
+}
