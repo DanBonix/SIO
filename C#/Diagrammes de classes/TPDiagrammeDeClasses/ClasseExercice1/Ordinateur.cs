9d6cccb541ba9ecb3af79e8092e581ff22596f9b
DanBonix <danheavydrive@gmail.com>
2017-12-05 19:48:45 +0100

    Add files via upload

---
+++ C#/Diagrammes de classes/TPDiagrammeDeClasses/ClasseExercice1/Ordinateur.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasseExercice1
+{
+    class Ordinateur
+    {
+        private string nom;
+        private int puissance;
+        private int tailleMemoire;
+
+        public Ordinateur(string nom, int puissance, int tailleMemoire)
+        {
+            this.nom = nom;
+            this.puissance = puissance;
+            this.tailleMemoire = tailleMemoire;
+        }
+
+    }
+}
